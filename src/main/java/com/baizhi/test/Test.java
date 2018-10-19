b84d60bfd9dc91d4b182c4e6afc40eb700102b40
kou <1094684691@qq.com>
2018-10-19 19:54:22 +0800

    第一次

--- src/main/java/com/baizhi/test/Test.java
+++ src/main/java/com/baizhi/test/Test.java
@@ -6,5 +6,6 @@ package com.baizhi.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("你好");
+        System.out.println("______________________________________________________________");
     }
 }
